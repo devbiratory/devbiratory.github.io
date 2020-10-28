3d9a63274a9f54dde1b4fc0640b345cd26e4cff3
sdei-devbir <devbir.singh@smartdatainc.net>
2020-10-28 15:36:59 +0530

    Updates

---
+++ precache-manifest.0816a8bf40b15a62f80687749beb584e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a8c0a29e8a42aae30c811016270c9c4",
+    "url": "/devbiratory/devbiratory.github.io.git/index.html"
+  },
+  {
+    "revision": "93325f7def3b91760141",
+    "url": "/devbiratory/devbiratory.github.io.git/static/css/main.08624717.chunk.css"
+  },
+  {
+    "revision": "90c31a35118bbc7d6188",
+    "url": "/devbiratory/devbiratory.github.io.git/static/js/2.985e6dc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/devbiratory/devbiratory.github.io.git/static/js/2.985e6dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93325f7def3b91760141",
+    "url": "/devbiratory/devbiratory.github.io.git/static/js/main.83a15ad0.chunk.js"
+  },
+  {
+    "revision": "02a230426eb41055fa78",
+    "url": "/devbiratory/devbiratory.github.io.git/static/js/runtime-main.58e84283.js"
+  }
+]);
